4d668a1361800f63bb5cbc6aee502183b1621ec3
Tom Kennedy <tkennedy@smu.edu>
2016-07-27 18:38:44 -0500

    changes

---
+++ js/media.js
@@ -0,0 +1,48 @@
+console.log('in media.js');
+
+//var root_url = "http://localhost:8888/hmpg/web/api/index.php/";
+var write_url = "web/api/write.php";
+
+function saveData(){
+	console.log('Saving data!!!');
+	console.log(globalData);
+
+	$.ajax
+	    ({
+	        type: "POST",
+	        dataType : 'json',
+	        async: false,
+	        url: write_url,
+	        data: { data: JSON.stringify(globalData) },
+	        success: function () {alert("Thanks!"); },
+	        failure: function() {alert("Error!");}
+	});
+}
+
+var globalData = {};
+
+$.getJSON( "web/info.json", function( data ) {
+  console.log(data);
+  globalData = data;
+  });
+
+function updateLocalStorage(userId){
+	var accts = globalData.Account;
+
+	console.log('looking for userid ' + userId);
+
+	var user = $.grep(
+	                accts, 
+	                function (item,index) { 
+	                	console.log(item);
+	                    return item.userId == userId; 
+	                });
+
+	console.log(user);
+		//alert('email matched password!');
+	$.each(user[0], function(key, value){
+		localStorage.setItem(key, value);
+	});
+	window.location = 'hmpg.html';
+	
+}
